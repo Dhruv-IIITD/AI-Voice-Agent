a7523a0cec012ccd7b56bc85db5227d701891d57
ashif22288 <mdasif22288@iiitd.ac.in>
2026-04-03 16:22:50 +0530

    Fix CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,7 +14,8 @@ app = FastAPI(title=settings.app_name)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[settings.frontend_origin],
+    allow_origins=[ "https://ai-voice-agent-production-a0a7.up.railway.app",
+                    settings.frontend_origin],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
